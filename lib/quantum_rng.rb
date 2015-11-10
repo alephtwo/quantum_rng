e6e3a3dc4b3e8575fa65f5d653f671d4cc764587
Ben Christianson <thhuntertgm@gmail.com>
2015-11-10 12:55:31 -0600

    Added backup capability if ANU ever goes down

--- lib/quantum_rng.rb
+++ lib/quantum_rng.rb
@@ -1,19 +1,33 @@
 require 'quantum_rng/version'
+require 'securerandom'
 require 'net/http'
 require 'json'
 
 # QuantumRNG gets random numbers from the ANU Quantum RNG API.
 module QuantumRNG
   def self.uint8(count = 1)
-    make_request(length: count, type: 'uint8')
+    make_request(
+      -> { Array.new(count).map { SecureRandom.random_number 255 } },
+      length: count,
+      type: 'uint8'
+    )
   end
 
   def self.uint16(count = 1)
-    make_request(length: count, type: 'uint16')
+    make_request(
+      -> { Array.new(count).map { SecureRandom.random_number(65_535) } },
+      length: count,
+      type: 'uint16'
+    )
   end
 
   def self.hex16(block_size, count = 1)
-    make_request(length: count, type: 'hex16', size: block_size)
+    make_request(
+      -> { Array.new(count).map { SecureRandom.hex block_size } },
+      length: count,
+      type: 'hex16',
+      size: block_size
+    )
   end
 
   def self.random(count = 1)
@@ -30,14 +44,16 @@ module QuantumRNG
 
   private
 
-  def self.make_request(queries)
+  def self.make_request(backup, queries)
     q = URI.encode_www_form(queries)
     uri = URI.parse("http://qrng.anu.edu.au/API/jsonI.php?#{q}")
     packet = JSON.parse(Net::HTTP.get(uri), symbolize_names: true)
     if packet[:success]
       packet[:data]
     else
-      fail "[ERROR] #{JSON.pretty_generate(packet)}"
+      backup.call
     end
+  rescue
+    backup.call
   end
 end
